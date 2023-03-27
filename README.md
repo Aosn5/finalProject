6360a7548c36cb36ec9133ea5c10e41697c4209c
Aosn5 <byaosn@gmail.com>
2023-03-27 03:25:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+
+Dynamic Landing Page project
+
+ The page dynamically creates a navigation menu based on the number of sections present in the page. It also highlights the active section and its corresponding navigation menu item while scrolling.
+
+Features
+
+Navigation menu dynamically created for each section.
+Clicking on a navbar smoothly scrolls to its corresponding section.
+The active section and its corresponding sections are highlighted while scrolling.
+
+Technologies Used
+
+HTML
+CSS
+JavaScript
+Installation
+
+Clone the repository
+bash
+Copy code
+git clone https://github.com/username/repository.git
+Open the index.html file in your preferred browser
+
+Usage
+
+Scroll through the page and notice how the active sections are highlighted.
+Click on a menu item to smoothly scroll to its corresponding section.
+
+Credits
+This project was completed as part of the Udacity Front End Web Developer Nanodegree program. The starter code was provided by Udacity, and additional features were added by me.
+
+
---
+++ app.js
@@ -0,0 +1,73 @@
+// stored the body and navbar elements in variables
+// Created the navbar and section lists
+const body = document.querySelector('body');
+const nav = document.querySelector('.navbar__menu');
+const options = document.getElementById('navbar__list');
+let sectionList = ['Section 1', 'Section 2', 'Section 3', 'Section 4'];
+const fragment = document.createDocumentFragment(); //fragment to add all the navbar list items
+
+// Loop through and create a navbar list item
+for (let i = 0; i < sectionList.length; i++) {
+  const li = document.createElement('li');
+  const aLink = document.createElement('a');
+  const sectionId = `section${i+1}`; 
+  aLink.textContent = sectionList[i];
+  aLink.className = 'menu__link';
+  li.className = 'nav-active';
+  aLink.setAttribute('href', `#${sectionId}`);
+  li.appendChild(aLink); 
+  fragment.appendChild(li);  
+  const section = document.createElement('section');
+  section.setAttribute('id', sectionId);
+  body.appendChild(section);
+
+} 
+// Get all the navbar list and links
+const sections = document.querySelectorAll('navbar__list');
+const aLinks = document.querySelectorAll('.menu__link');
+
+aLinks.forEach((aLink) => {
+// Add event listener to each item
+    aLink.addEventListener('click', (ev) => {
+//smooth scroll
+const sectionId = aLink.getAttribute('href');
+    document.querySelector(sectionId).scrollIntoView(
+      {behavior: "smooth"});
+           
+// Remove & add the 'nav-active' class 
+    aLinks.forEach((link) => link.classList.remove('nav-active'));
+      aLink.classList.add('nav-active'); 
+// Prevent the default
+      ev.preventDefault(); 
+    });
+  });
+// Add event listener to window to display viewport
+// Loop through each section in the viewport
+
+window.addEventListener('scroll', () => {
+
+    sections.forEach((section, i) => {
+
+   if(
+        sections[i].getBoundingClientRect().top <= 150 &&
+        sections[i].getBoundingClientRect().bottom >= 150
+    ){
+        sections[i].classList.add('your-active-class');
+        aLinks[i].classList.add('nav-active');
+    } 
+    else {
+        sections[i].classList.remove('your-active-class');
+        aLinks[i].classList.remove('nav-active');
+        }
+    });
+});
+
+// Append the navbar items to the navbar
+options.appendChild(fragment);
+
+
+
+
+
+
+
